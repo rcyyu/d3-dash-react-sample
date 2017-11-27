8a35f1e629bc77cce3e8b1b9665efa58b117c6ba
Ron Yu <ronald.c.yu@gmail.com>
2017-11-27 14:35:04 -0500

    Updated D3 version syntax

--- svg-example/components/ExampleComponent.react.js
+++ svg-example/components/ExampleComponent.react.js
@@ -1,6 +1,39 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import {select} from 'd3-selection';
 import ReactFauxDOM from 'react-faux-dom';
+
+var Box = React.createClass({
+	getInitialState: function () {
+		return {
+			mouseOver: false
+		}
+	},
+	render: function () {
+		var self = this
+		var svg = select(ReactFauxDOM.createElement('svg'))
+		.attr('width', 300)
+		.attr('height', 300)
+		svg
+		.append('rect')
+		.attr('width', 300)
+		.attr('height', 300)
+		.style('fill', this.state.mouseOver ? 'red' : 'green')
+		.on('mouseover', function () {
+			self.setState({
+				mouseOver: true
+			})
+		})
+		.on('mouseout', function () {
+			self.setState({
+				mouseOver: false
+			})
+		})
+
+		return svg.node().toReact()
+	}
+})
 
 /**
  * ExampleComponent is an example component.
@@ -10,68 +43,11 @@ import ReactFauxDOM from 'react-faux-dom';
  * which is editable by the user.
  */
 class ExampleComponent extends Component {
-    //    componentDidMount() {
-    //        const faux = this.props.connectFauxDOM('div', 'id')
-    //        select(faux)
-    //            .append('div')
-    //            .html('Hello world')
-    //        this.props.animateFauxDOM(800)
-    //    }
     render() {
-        var Box = React.createClass({
-            getInitialState: function () {
-                return {
-                    mouseOver: false
-                }
-            },
-            render: function () {
-                var self = this
-                var svg = select(ReactFauxDOM.createElement('svg'))
-                .attr({
-                    width: 300,
-                    height: 300
-                })
-                svg
-                    .append('rect')
-                    .attr({
-                    width: 300,
-                    height: 300,
-                    fill: this.state.mouseOver ? 'red' : 'green'
-                })
-                    .on('mouseover', function () {
-                    self.setState({
-                        mouseOver: true
-                    })
-                })
-                    .on('mouseout', function () {
-                    self.setState({
-                        mouseOver: false
-                    })
-                })
-
-                return svg.node().toReact()
-            }
-        })
-        return (
-            <div className='renderedD3'>
-            {React.createElement(Box)}
-            </div>
-        )}
-//        const {id} = this.props;
-//
-//        return (
-//            <div className='renderedD3'>
-//                {id}
-//            </div>
-//        );
+		return (
+			<Box></Box>
+		)
+	}
 }
-
-
-//ExampleComponent.propTypes = {
-//    /**
-//     * The ID used to identify this compnent in Dash callbacks
-//     */
-//    id: PropTypes.string
-//};
 
 export default ReactFauxDOM.withFauxDOM(ExampleComponent)
